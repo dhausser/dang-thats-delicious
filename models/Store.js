3213b503a8f8c500a3081daa80fb1a6cf6896c7f
Davy Hausser <davy.hausser@gmail.com>
2018-11-10 20:13:54 +0000

    first commit

---
+++ models/Store.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name!',
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true,
+  },
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point',
+    },
+    coordinates: [{
+      type: Number,
+      required: 'You must supply coordinates!',
+    }],
+    address: {
+      type: String,
+      required: 'You must supply and address!',
+    },
+  },
+  photo: String,
+});
+
+storeSchema.pre('save', async function (next) {
+  if (!this.isModified('name')) {
+    next(); // skip it
+    return; // stop this function from running
+  }
+  this.slug = slug(this.name);
+  // find other stores the have a slug of wes, wes-1, wes-2
+  const slugRegExp = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegExp });
+  if (storesWithSlug) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`
+  }
+  next();
+  // TODO make more resiliant so slugs are unique
+});
+
+storeSchema.statics.getTagsList = function() {
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+    { $sort: { count: -1 } }
+  ]);
+}
+
+module.exports = mongoose.model('Store', storeSchema);
